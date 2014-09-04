3789f3c63149e844eda137060ae771cd0a74484e
Daniel Cano <danielcanog@gmail.com>
2014-09-04 17:02:59 -0500

    Comentando

--- src/spaceWarsConsole/ship.h
+++ src/spaceWarsConsole/ship.h
@@ -5,7 +5,10 @@
 class ship : public graphicObjects
 {
 private:
+    //This function initializes the object.
+    //This function must be executed in any constructor
     void initGraphicObject(point pos, float speed, char dir, int flR, int flL, int flT, int flB);
+
 public:
     ship();
     ship(point pos, float speed, char dir, int flR, int flL, int flT, int flB);
